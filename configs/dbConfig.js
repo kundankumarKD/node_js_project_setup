a108a2dd66d0edbb0ed46424044cc7016a76f1e1
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-03-27 18:13:00 +0530

    Update dbConfig.js

---
+++ configs/dbConfig.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'database_dev',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql' // or 'postgres', 'sqlite', etc.
+  },
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASS_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
