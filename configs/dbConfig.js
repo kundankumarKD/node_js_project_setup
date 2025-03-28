412440fd6c819acad6a986469d7e2ec6d60a85a0
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-03-28 10:44:49 +0530

    Update dbConfig.js

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'database_dev',
-    host: process.env.DB_HOST || '127.0.0.1',
+    password: process.env.DB_PASS || 'password',
+    database: process.env.DB_NAME || 'php_crud_api',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql' // or 'postgres', 'sqlite', etc.
   },
   test: {
