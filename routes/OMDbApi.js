34411af9480eb7a0286a71533f215abdfffdccb5
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-10-19 10:56:09 +0530

    Update OMDbApi.js

--- routes/OMDbApi.js
+++ routes/OMDbApi.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const OMDbApiController = require('../controllers/OMDbApiController')
 
 router.route('/list-movies').get(OMDbApiController.listMovies)
+router.route('/test-connection').get(OMDbApiController.testConnection)
+
 
 exports.Router = router
